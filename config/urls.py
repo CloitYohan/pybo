97349c4acaac3e08e929560c6232b952fa7363ed
도요한 <ydo@cloit.com>
2024-01-05 16:19:39 +0900

    ?

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
 ]
-handler404 = 'common.views.page_not_found'
-handler500 = 'common.views.internal_server_error'
+#handler404 = 'common.views.page_not_found'
+#handler500 = 'common.views.internal_server_error'
