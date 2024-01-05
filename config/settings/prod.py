bc0d216616831dfe0727edf34dd310adc1a46243
도요한 <ydo@cloit.com>
2024-01-05 17:14:26 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.202.168.28']
+ALLOWED_HOSTS = ['43.202.168.28', 'cloit.site']
 STATIC_ROOT = BASE_DIR / 'static/' #BASE_DIR = /home/ubuntu/projects/mysite
 STATICFILES_DIRS = []
 DEBUG = False
